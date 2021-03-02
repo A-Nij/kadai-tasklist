5fd1de9e54144ae51edd3c3a96804f809ed610f0
A-Niji <sun.nika.fine.15@gmail.com>
2021-03-02 06:55:12 +0000

    KadaiTasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
         @tasks = Task.all
     end
 
     def show
-        @task = Task.find(params[:id])
+
     end
 
     def new
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
 
     def create
         @task = Task.new(task_params)
+
 
         if @task.save
             flash[:success] = "Task が正常に保存されました"
@@ -25,11 +26,11 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @task = Task.find(params[:id])
+
     end
 
     def update
-        @task = Task.find(params[:id])
+
 
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -40,7 +41,7 @@ class TasksController < ApplicationController
         end
     end
     def destroy
-        @task = Task.find(params[:id])
+
         @task.destroy
 
         flash[:success] = 'Task は正常に削除されました'
@@ -48,10 +49,17 @@ class TasksController < ApplicationController
     end
 
     private
-    
-    
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+
 
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 end
+
+
+
